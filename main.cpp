a692e19e419171179e896c676cd2fd5f7de4be03
Rzesiu <250994@edu.p.lodz.pl>
2024-06-21 14:28:13 +0200

    fixed

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "text.h"
+void menu(){
+    printf("1. Enter Text\n");
+    printf( "2. Print Text\n");
+    printf( "3. Text Length\n");
+    printf( "4. Change To Big Letters\n");
+    printf( "5. Change To Small Letters\n");
+    printf( "6. EXIT\n");
+    printf("Select an option:\n");
+}
+int main() {
+    char text[100] = " ";
+    int choice = 0 ;
+    do{
+        menu();
+        scanf("%d",&choice);
+        clear();
+        switch(choice)
+        {
+            case 1:
+                enterText(text);
+            break;
+            case 2:
+                printText(text);
+            break;
+            case 3:
+                printf("Text length = %d\n", count(text));
+            break;
+            case 4:
+                bigLetters(text);
+            puts("Change To Big Letters\n");
+            break;
+            case 5:
+                smallLetters(text);
+            puts("Change To Small Letters\n");
+            break;
+            case 6:
+                break;
+        }
+    }while( choice!=6);
+}
---
+++ text.h
@@ -0,0 +1,10 @@
+
+#ifndef TEXT_H
+#define TEXT_H
+void enterText(char *text);
+void printText(char *text);
+void clear(void);
+int count(char *text);
+void bigLetters(char *string);
+void smallLetters(char *string);
+#endif //TEXT_H
